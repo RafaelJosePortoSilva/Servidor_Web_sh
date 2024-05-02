d17201ab179da4788e4c07fa95754d0dbac2df81
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2024-05-02 19:51:03 -0300

    erros 2

--- configura_server_web.sh
+++ configura_server_web.sh
@@ -12,11 +12,11 @@ sudo apt install unzip
 
 # Baixa aplicacao para diretorio tmp
 sudo rm -rf /tmp/*.zip
-sudo wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip /tmp
+sudo wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip /tmp/
 
 # Copia tmp para pasta apache
 sudo rm -rf /var/www/html/*
-sudo cp /tmp/* /var/www/html -rf
+sudo cp /tmp/* /var/www/html/ -R
 
 sudo unzip /var/www/html/main.zip
 sudo rm -rf main.zip
